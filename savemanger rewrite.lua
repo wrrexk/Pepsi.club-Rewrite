896b48f5a054c3bd5692bd7b89a6506edf8c2611
wrrexk <wrrexk@gmail.com>
2025-01-06 00:30:52 -0500

    Update savemanger rewrite.lua

--- savemanger rewrite.lua
+++ savemanger rewrite.lua
@@ -133,7 +133,7 @@ local SaveManager = {} do
 
 	function SaveManager:IgnoreThemeSettings()
 		self:SetIgnoreIndexes({ 
-			"BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor", -- themes
+			"", -- themes
 			"ThemeManager_ThemeList", 'ThemeManager_CustomThemeList', 'ThemeManager_CustomThemeName', -- themes
 		})
 	end
